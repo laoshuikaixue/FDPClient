974d49b0485680b8babd724acf2aa8c696eae46b
LaoShui <3048087505@qq.com>
2022-03-16 10:39:13 +0800

    ADD MotionSpeed

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MotionPacket.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MotionPacket.kt
@@ -7,6 +7,7 @@ import net.ccbluex.liquidbounce.value.FloatValue
 
 class MotionPacket : SpeedMode("MotionPacket") {
     private val MotionValue = FloatValue("${valuePrefix}Motion", 0.17f, 0.1f, 2f)
+
     override fun onMotion(event: MotionEvent) {
         MovementUtils.setMotion(MotionValue.get().toDouble())
     }
