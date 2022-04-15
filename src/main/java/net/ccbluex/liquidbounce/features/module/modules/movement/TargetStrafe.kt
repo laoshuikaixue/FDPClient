bc557ac8e2e86d11eafe464e01865e5d8a1a3089
LaoShui <3048087505@qq.com>
2022-04-15 19:52:48 +0800

    better

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
@@ -1,219 +1,132 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/UnlegitMC/FDPClient/
+ */
+//QQ3040438566 FengXiaoGod
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.LiquidBounce
-import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
-import net.ccbluex.liquidbounce.event.*
-import net.ccbluex.liquidbounce.value.BoolValue
-import net.ccbluex.liquidbounce.value.FloatValue
-import net.ccbluex.liquidbounce.value.IntegerValue
-import net.ccbluex.liquidbounce.value.ListValue
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.MoveEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
+import net.ccbluex.liquidbounce.utils.PlayerUtils
 import net.ccbluex.liquidbounce.utils.RotationUtils
-import net.ccbluex.liquidbounce.utils.render.ColorUtils
-import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.minecraft.entity.Entity
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
 import net.minecraft.entity.EntityLivingBase
-import net.minecraft.client.renderer.GlStateManager
-import net.minecraft.util.AxisAlignedBB
-import org.lwjgl.opengl.GL11
-import java.awt.Color
+import kotlin.math.*
 
-@ModuleInfo(name = "TargetStrafe", category = ModuleCategory.MOVEMENT)
+
+@ModuleInfo(name = "TargetStrafe",  category = ModuleCategory.MOVEMENT)
 class TargetStrafe : Module() {
-    private val radius = FloatValue("Radius", 2.0f, 0.1f, 4.0f)
-    private val render = BoolValue("Render", true)
-    private val alwaysRender = BoolValue("Always-Render", true).displayable { render.get() }
-    private val modeValue = ListValue("KeyMode", arrayOf("Jump", "None"), "None")
-    private val safewalk = BoolValue("SafeWalk", true)
-    val thirdPerson = BoolValue("ThirdPerson", true)
-    private val colorType = ListValue("Color", arrayOf("Custom", "Dynamic", "Rainbow", "Rainbow2", "Sky", "Fade"), "Custom")
-    private val redValue = IntegerValue("Red", 255, 0, 255)
-    private val greenValue = IntegerValue("Green", 255, 0, 255)
-    private val blueValue = IntegerValue("Blue", 255, 0, 255)
-    private val saturationValue = FloatValue("Saturation", 0.7F, 0F, 1F)
-    private val brightnessValue = FloatValue("Brightness", 1F, 0F, 1F)
-    private val accuracyValue = IntegerValue("Accuracy", 0, 0, 59)
-    private val thicknessValue = FloatValue("Thickness", 1F, 0.1F, 5F)
-    private val outLine = BoolValue("Outline", true)
-    private val expMode = BoolValue("ExperimentalSpeed", false)
-    private lateinit var killAura: KillAura
-    private lateinit var speed: Speed
-    private lateinit var fly: Fly
+    private val thirdPersonViewValue = BoolValue("ThirdPersonView", false)
+    private val radiusValue = FloatValue("Radius", 0.1f, 0.5f, 5.0f)
+    private val killAura = LiquidBounce.moduleManager[KillAura::class.java]
+    private var direction = -1
 
-    var direction: Int = 1
-    var lastView: Int = 0
-    var hasChangedThirdPerson: Boolean = true
-
-    override fun onInitialize() {
-        killAura = LiquidBounce.moduleManager.getModule(KillAura::class.java) as KillAura
-        speed = LiquidBounce.moduleManager.getModule(Speed::class.java) as Speed
-        fly = LiquidBounce.moduleManager.getModule(Fly::class.java) as Fly
-    }
-
-    override fun onEnable() {
-        hasChangedThirdPerson = true
-        lastView = mc.gameSettings.thirdPersonView
-    }
-
-    @EventTarget
-    fun onMotion(event: MotionEvent) {
-        if (thirdPerson.get()) { // smart change back lol
-            if (canStrafe) {
-                if (hasChangedThirdPerson) lastView = mc.gameSettings.thirdPersonView
-                mc.gameSettings.thirdPersonView = 1
-                hasChangedThirdPerson = false
-            } else if (!hasChangedThirdPerson) {
-                mc.gameSettings.thirdPersonView = lastView
-                hasChangedThirdPerson = true
-            }
-        }
-
-        if (event.eventState == EventState.PRE) {
-            if (mc.thePlayer.isCollidedHorizontally)
-                this.direction = -this.direction
-
-            if (mc.gameSettings.keyBindLeft.pressed)
-                this.direction = 1
-
-            if (mc.gameSettings.keyBindRight.pressed)
-                this.direction = -1
-        }
-    }
-
+    /**
+     *
+     * @param event MoveEvent
+     */
     @EventTarget
     fun onMove(event: MoveEvent) {
-        if (canStrafe) {
-            strafe(event, MovementUtils.getSpeed3(event.x, event.z))
+        if(!canStrafe) return
+        var aroundVoid = false
+        for (x in -1..0) for (z in -1..0) if (isVoid(x, z)) aroundVoid = true
 
-            if (safewalk.get() && checkVoid())
-                event.isSafeWalk = true
-        }
-    }
+        var yaw = RotationUtils.getRotationFromEyeHasPrev(killAura!!.target).yaw
 
-    fun strafe(event: MoveEvent, moveSpeed: Double) {
-        if (killAura.target == null) return
+        if (mc.thePlayer.isCollidedHorizontally || aroundVoid) direction *= -1
 
-        val target = killAura.target!!
-        val rotYaw = RotationUtils.getRotationsEntity(target).yaw
+        var targetStrafe = (if (mc.thePlayer.moveStrafing != 0F) mc.thePlayer.moveStrafing * direction else direction.toFloat())
+        if (!PlayerUtils.isBlockUnder()) targetStrafe = 0f
 
-        val forward = if (mc.thePlayer.getDistanceToEntity(target) <= radius.get()) 0.0 else 1.0
-        var modifySpeed = if (expMode.get()) maximizeSpeed(target, moveSpeed, killAura.rangeValue.get()) else moveSpeed
+        val rotAssist = 45 / mc.thePlayer.getDistanceToEntity(killAura.target)
+        val moveAssist = (45f / getStrafeDistance(killAura.target!!)).toDouble()
 
-        MovementUtils.setSpeed(event, modifySpeed, rotYaw, direction.toDouble(), forward)
-    }
+        var mathStrafe = 0f
 
-    private fun maximizeSpeed(ent: EntityLivingBase, speed: Double, range: Float): Double {
-        mc.thePlayer ?: return 0.0
-        /*val dist = mc.thePlayer.getDistanceToEntity(ent).toDouble()
-        val maxDist = (range * range).coerceAtMost(radius.get() * radius.get() - 0.25f).toDouble() - (dist * dist)*/
-
-        return speed.coerceIn(0.0, /*maxDist*/range.toDouble())
-    }
-
-    val keyMode: Boolean
-        get() = when (modeValue.get().toLowerCase()) {
-            "jump" -> mc.gameSettings.keyBindJump.isKeyDown
-            "none" -> mc.thePlayer.movementInput.moveStrafe != 0f || mc.thePlayer.movementInput.moveForward != 0f
-            else -> false
+        if (targetStrafe > 0) {
+            if ((killAura.target!!.entityBoundingBox.minY > mc.thePlayer.entityBoundingBox.maxY || killAura.target!!.entityBoundingBox.maxY < mc.thePlayer.entityBoundingBox.minY) && mc.thePlayer.getDistanceToEntity(
+                    killAura.target!!
+                ) < radiusValue.get()
+            ) yaw += -rotAssist
+            mathStrafe += -moveAssist.toFloat()
+        } else if (targetStrafe < 0) {
+            if ((killAura.target!!.entityBoundingBox.minY > mc.thePlayer.entityBoundingBox.maxY || killAura.target!!.entityBoundingBox.maxY < mc.thePlayer.entityBoundingBox.minY) && mc.thePlayer.getDistanceToEntity(
+                    killAura.target!!
+                ) < radiusValue.get()
+            ) yaw += rotAssist
+            mathStrafe += moveAssist.toFloat()
         }
 
-    val canStrafe: Boolean
-        get() = (state && (speed.state || fly.state) && killAura.state && killAura.target != null && !mc.thePlayer.isSneaking && keyMode)
+        val doSomeMath = doubleArrayOf(
+            cos(Math.toRadians((yaw + 90f + mathStrafe).toDouble())),
+            sin(Math.toRadians((yaw + 90f + mathStrafe).toDouble()))
+        )
+        val moveSpeed = sqrt(event.x.pow(2.0) + event.z.pow(2.0))
+
+        val asLast = doubleArrayOf(
+            moveSpeed * doSomeMath[0],
+            moveSpeed * doSomeMath[1]
+        )
+
+        event.x = asLast[0]
+        event.z = asLast[1]
+        //        if (mc.thePlayer.isCollidedHorizontally || checkVoid()) direction = if (direction == 1) -1 else 1
+//        if(checkVoid() && canStrafe) return
+//        if (mc.gameSettings.keyBindLeft.isKeyDown) {
+//            direction = 1
+//        }
+//        if (mc.gameSettings.keyBindRight.isKeyDown) {
+//            direction = -1
+//        }
+//
+//        if (!isVoid(0, 0) && canStrafe) {
+//            MovementUtils.setSpeed(
+//                event,
+//                sqrt(event.x.pow(2.0) + event.z.pow(2.0)),
+//                RotationUtils.toRotation(RotationUtils.getCenter(killAura.target?.entityBoundingBox), true).yaw,
+//                direction.toDouble(),
+//                if (mc.thePlayer.getDistanceToEntity(killAura.target) <= radiusValue.get()) 0.0 else 1.0
+//            )
+//        }
+        if (!thirdPersonViewValue.get()) return
+        mc.gameSettings.thirdPersonView = if (canStrafe) 3 else 0
+    }
+
+    private val canStrafe: Boolean
+        get() = killAura!!.state && killAura.target != null && !mc.thePlayer.isSneaking
 
     private fun checkVoid(): Boolean {
-        for (x in -1..0) {
-            for (z in -1..0) {
-                if (isVoid(x, z)) {
-                    return true
-                }
-            }
-        }
+        for (x in -2..2) for (z in -2..2) if (isVoid(x, z)) return true
         return false
     }
 
-    private fun isVoid(X: Int, Z: Int): Boolean {
-        if (mc.thePlayer.posY < 0.0) {
-            return true
-        }
+    private fun getStrafeDistance(target: EntityLivingBase): Float {
+        return (mc.thePlayer.getDistanceToEntity(target) - radiusValue.get()).coerceAtLeast(
+            mc.thePlayer.getDistanceToEntity(
+                target
+            ) - (mc.thePlayer.getDistanceToEntity(target) - radiusValue.get() / (radiusValue.get() * 2))
+        )
+    }
+
+    private fun isVoid(xPos: Int, zPos: Int): Boolean {
+        if (mc.thePlayer.posY < 0.0) return true
         var off = 0
         while (off < mc.thePlayer.posY.toInt() + 2) {
-            val bb: AxisAlignedBB = mc.thePlayer.entityBoundingBox.offset(X.toDouble(), (-off).toDouble(), Z.toDouble())
-            if (mc.theWorld!!.getCollidingBoundingBoxes(mc.thePlayer as Entity, bb).isEmpty()) {
+            val bb = mc.thePlayer.entityBoundingBox.offset(xPos.toDouble(), -off.toDouble(), zPos.toDouble())
+            if (mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, bb).isEmpty()) {
                 off += 2
                 continue
             }
             return false
-            off += 2
         }
         return true
     }
 
-    @EventTarget
-    fun onRender3D(event: Render3DEvent) {
-        val target = killAura.target
-        if ((canStrafe || alwaysRender.get()) && render.get()) {
-            target?:return
-            GL11.glPushMatrix()
-            GL11.glTranslated(
-                target.lastTickPosX + (target.posX - target.lastTickPosX) * mc.timer.renderPartialTicks - mc.getRenderManager().renderPosX,
-                target.lastTickPosY + (target.posY - target.lastTickPosY) * mc.timer.renderPartialTicks - mc.getRenderManager().renderPosY,
-                target.lastTickPosZ + (target.posZ - target.lastTickPosZ) * mc.timer.renderPartialTicks - mc.getRenderManager().renderPosZ
-            )
-            GL11.glEnable(GL11.GL_BLEND)
-            GL11.glEnable(GL11.GL_LINE_SMOOTH)
-            GL11.glDisable(GL11.GL_TEXTURE_2D)
-            GL11.glDisable(GL11.GL_DEPTH_TEST)
-            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
-            GL11.glRotatef(90F, 1F, 0F, 0F)
-
-            if (outLine.get()) {
-                GL11.glLineWidth(thicknessValue.get() + 1.25F)
-                GL11.glColor3f(0F, 0F, 0F)
-                GL11.glBegin(GL11.GL_LINE_LOOP)
-
-                for (i in 0..360 step 60 - accuracyValue.get()) { // You can change circle accuracy  (60 - accuracy)
-                    GL11.glVertex2f(Math.cos(i * Math.PI / 180.0).toFloat() * radius.get(), (Math.sin(i * Math.PI / 180.0).toFloat() * radius.get()))
-                }
-
-                GL11.glEnd()
-            }
-
-            val rainbow2 = ColorUtils.LiquidSlowly(System.nanoTime(), 0, saturationValue.get(), brightnessValue.get())
-            val sky = RenderUtils.skyRainbow(0, saturationValue.get(), brightnessValue.get())
-            val fade = ColorUtils.fade(Color(redValue.get(), greenValue.get(), blueValue.get()), 0, 100)
-
-            GL11.glLineWidth(thicknessValue.get())
-            GL11.glBegin(GL11.GL_LINE_LOOP)
-
-            for (i in 0..360 step 60 - accuracyValue.get()) { // You can change circle accuracy  (60 - accuracy)
-                when (colorType.get()) {
-                    "Custom" -> GL11.glColor3f(redValue.get() / 255.0f, greenValue.get() / 255.0f, blueValue.get() / 255.0f)
-                    "Dynamic" -> if (canStrafe) GL11.glColor4f(0.25f, 1f, 0.25f, 1f) else GL11.glColor4f(1f, 1f, 1f, 1f)
-                    "Rainbow" -> {
-                        val rainbow = Color(RenderUtils.getNormalRainbow(i, saturationValue.get(), brightnessValue.get()))
-                        GL11.glColor3f(rainbow.red / 255.0f, rainbow.green / 255.0f, rainbow.blue / 255.0f)
-                    }
-                    "Rainbow2" -> GL11.glColor3f(rainbow2!!.red / 255.0f, rainbow2!!.green / 255.0f, rainbow2!!.blue / 255.0f)
-                    "Sky" -> GL11.glColor3f(sky.red / 255.0f, sky.green / 255.0f, sky.blue / 255.0f)
-                    else -> GL11.glColor3f(fade.red / 255.0f, fade.green / 255.0f, fade.blue / 255.0f)
-                }
-                GL11.glVertex2f(Math.cos(i * Math.PI / 180.0).toFloat() * radius.get(), (Math.sin(i * Math.PI / 180.0).toFloat() * radius.get()))
-            }
-
-            GL11.glEnd()
-
-            GL11.glDisable(GL11.GL_BLEND)
-            GL11.glEnable(GL11.GL_TEXTURE_2D)
-            GL11.glEnable(GL11.GL_DEPTH_TEST)
-            GL11.glDisable(GL11.GL_LINE_SMOOTH)
-
-            GL11.glPopMatrix()
-
-            GlStateManager.resetColor()
-            GL11.glColor4f(1F, 1F, 1F, 1F)
-        }
-    }
 }
